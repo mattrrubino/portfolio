f0067a481b061178baa0d4e0fe29c70be305fb4b
Matthew Rubino <mrrubino@buffalo.edu>
2024-08-11 15:58:00 -0400

    Fix container appear

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default function Container({ children }) {
   return (
-    <div className="lg:snap-start h-[calc(100vh-4rem)] relative border-black border-2 border-t-0">
+    <div className="lg:snap-start h-[calc(100vh-4rem)] relative border-black border-2 border-t-0 pt-1">
       {children}
     </div>
   )
